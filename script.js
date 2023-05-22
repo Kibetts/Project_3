375c558d3a64bbbbdee80f9d3c0590cc2c2bb79d
Brian Kibet <kibetsbrian@gmail.com>
2023-05-22 11:59:27 +0300

    initialize DOM  constants

---
+++ script.js
@@ -0,0 +1,8 @@
+const gameContainer = document.querySelector(".game-general");
+const timeValue = document.getElementById("time");
+const stopButton = document.getElementById("stop");
+const result = document.getElementById("result");
+const startButton = document.getElementById("start");
+const controls = document.querySelector(".controls-container");
+const moves = document.getElementById("moves-count");
+let tiles;
