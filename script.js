4a70e33fce3e0558acc8896310b1df2c9f0cdb3c
Brian Kibet <kibetsbrian@gmail.com>
2023-05-22 13:00:48 +0300

    define unmatched tile logic

--- script.js
+++ script.js
@@ -100,4 +100,18 @@ const matrixGenerator = (cardValues, size = 4) => {
 				result.innerHTML = `<h2>You Won</h2>
 			  <h4>Moves: ${movesCount}</h4>`;
 				stopGame();
+			  } else {
+          
+				let [tempFirst, tempSecond] = [activeTile, secondTile];
+				activeTile= false;
+				secondTile = false;
+				let delay = setTimeout(() => {
+				  tempFirst.classList.remove("flipped");
+				  tempSecond.classList.remove("flipped");
+				}, 900);
 			  }
+			}
+		  }
+		});
+	  });
+	};
