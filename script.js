a47559afec33ade5ac6f8fa9105a772a8f3582ad
Brian Kibet <kibetsbrian@gmail.com>
2023-05-22 12:19:16 +0300

    define variables and add images to javascript

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const startButton = document.getElementById("start");
 const controls = document.querySelector(".controls-container");
 const moves = document.getElementById("moves-count");
 let tiles;
+let interval;
+let activeTile = false;
+let secondTile = false;
+
+const pictures = [
+  { name: "bishop", image: "./bishop.png" },
+  { name: "king", image: "./king.png" },
+  { name: "knight", image: "./knight.png" },
+  { name: "pawny", image: "./pawny.png" },
+  { name: "queen", image: "./queen.png" },
+  { name: "rook", image: "./rook.png" },
+ 
+];
